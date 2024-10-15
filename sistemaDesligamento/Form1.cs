975f1b42c8898449787391c67abbf57da0567c4a
asboliveira <amauri_bessa@hotmail.com>
2024-10-15 20:16:52 -0300

    Teste Git

--- sistemaDesligamento/Form1.cs
+++ sistemaDesligamento/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;// Biblioteca para diagnosticos do sistema
+
 
 
 namespace sistemaDesligamento
